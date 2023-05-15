2310f2e579b39dbfe786af3aa33ef5c9db7c269c
Revan-Darth <nitro.Knight3080@gmail.com>
2023-05-15 17:37:14 +0700

    Adaptive for Solving

--- js/main.js
+++ js/main.js
@@ -216,3 +216,32 @@
 	} else {
 		document.querySelector('.header__phone-number').classList.remove('visually-hidden');
 	}
+
+
+// Tabs
+	const tabsBtn = document.querySelectorAll('.tabs__nav-btn');
+	const tabsItems = document.querySelectorAll('.tabs__item');
+
+	tabsBtn.forEach(onTabClick);
+
+	function onTabClick(item) {
+		item.addEventListener('click', function() {
+			let currentBtn = item;
+			let tabId = currentBtn.getAttribute('data-tab');
+			let currentTab = document.querySelector(tabId);
+
+			if ( ! currentBtn.classList.contains('_active') ) {
+				tabsBtn.forEach(function(item) {
+					item.classList.remove('_active')
+				});
+
+				tabsItems.forEach(function(item) {
+					item.classList.remove('_active')
+				});
+
+				currentBtn.classList.add('_active');
+				currentTab.classList.add('_active');
+			}
+		});
+	}
+
